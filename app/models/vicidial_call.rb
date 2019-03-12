073eb1010bc2abd2ec063c55f3723fc2a9f3f000
Braullio Vinnicius <braulliovg@gmail.com>
2019-03-12 16:48:45 -0300

    Linking model VicidialCall (Table Vicidial: asterisk.vicidial_log) in model VicidialLead (Table Vicidial: asterisk.vicidial_lead)

--- app/models/vicidial_call.rb
+++ app/models/vicidial_call.rb
@@ -1,4 +1,5 @@
 class VicidialCall < ApplicationRecord
   establish_connection(:vicidial_db)
   self.table_name = 'asterisk.vicidial_log'
+  belongs_to :vicidial_lead, primary_key: :lead_id, foreign_key: :lead_id
 end
--- app/models/vicidial_lead.rb
+++ app/models/vicidial_lead.rb
@@ -1,4 +1,5 @@
 class VicidialLead < ApplicationRecord
   establish_connection(:vicidial_db)
   self.table_name = 'asterisk.vicidial_list'
+  has_many :vicidial_calls, primary_key: :lead_id, foreign_key: :lead_id
 end
